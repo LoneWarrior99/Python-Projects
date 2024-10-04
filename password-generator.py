a3044b4fd9834e7edbff377ed9dc69ea5ab75fdd
Khang Nguyen <158310798+LoneWarrior99@users.noreply.github.com>
2024-10-04 13:38:54 -0500

    password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,27 +1,34 @@
-from agrparse import ArgumentParser
+from argparse import ArgumentParser
 import secrets
 import random
 import string
+import pyfiglet
+
+#Pretty banner
+ascii_banner = pyfiglet.figlet_format("Password Generator")
+print(ascii_banner)
+print("-" * 50)
+
 
 #Help Menu
 parser = ArgumentParser(
   prog= 'Password Generator.',
-  description= 'Generate passwords with this tool.'
-  epiloge= 'Thanks for checking this out!'
+  description= 'Generate passwords with this tool.',
+  epilog= 'Thanks for checking this out!'
 )
 
 
 #Adding arguments to the help menu
-parser.add_arguement("-n", "--numbers", default=0, help="Adds amount of numbers to your password", type=int)
-parser.add_arguement("-l", "--lowercase", default=0, help="Adds amount of lowercase characters to your password", type=int)
-parser.add_arguement("-u", "--uppercase", default=0, help="Adds amount of uppercase characters to your password", type=int)
-parser.add_arguement("-s", "--special-chars", default=0, help="Adds amount of special characters to your password", type=int)
-parser.add_arguement("-t", "--total-length", type=int, help="This specifies length of password and will generate a completely random password, *Will ignore all other syntaxes*.")
-parser.add_arguement("-a", "--amount", default=1, help="Adds amount of passwords you want.", type=int)
-parser.add_arguement("-o", "--output-file", help="Outputs file")
+parser.add_argument("-n", "--numbers", default=0, help="Adds amount of numbers to your password", type=int)
+parser.add_argument("-l", "--lowercase", default=0, help="Adds amount of lowercase characters to your password", type=int)
+parser.add_argument("-u", "--uppercase", default=0, help="Adds amount of uppercase characters to your password", type=int)
+parser.add_argument("-s", "--special-chars", default=0, help="Adds amount of special characters to your password", type=int)
+parser.add_argument("-t", "--total-length", type=int, help="This specifies length of password and will generate a completely random password, *Will ignore all other syntaxes*.")
+parser.add_argument("-a", "--amount", default=1, help="Adds amount of passwords you want.", type=int)
+parser.add_argument("-o", "--output-file", help="Outputs file")
 
 #Parses the command line arguments
-args = parser.parse_agrs()
+args = parser.parse_args()
 
 #List
 passwords = []
@@ -30,14 +37,14 @@ passwords = []
 for _ in range(args.amount):
   if args.total_length:
     #generates random password with this length
-    password.append("".join(
+    passwords.append("".join(
       [secrets.choice(string.digits + string.ascii_letters + string.punctuation)
-       for _ in range(args.total.length)]))
+       for _ in range(args.total_length)]))
     
   else:
     password = []
 
-    for _ in range(args.number):
+    for _ in range(args.numbers):
       password.append(secrets.choice(string.digits))
       
     for _ in range(args.uppercase):
